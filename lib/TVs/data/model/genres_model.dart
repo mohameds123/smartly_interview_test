0e7b70ccc8541e529777bfb4787324118124740e
mohamed saleh <mohameds123>
2023-01-05 23:47:43 +0200

    smartly

--- lib/TVs/data/model/genres_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:movies/TVs/domain/entities/tv_genres.dart';
-
-class TvGenresModel extends TvGenres {
-  const TvGenresModel({required int id, required String name})
-      : super(id: id, name: name);
-
-  factory TvGenresModel.fromJson(Map<String, dynamic> json) =>
-      TvGenresModel(id: json['id'], name: json['name']);
-}
